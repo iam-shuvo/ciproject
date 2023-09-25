58ed066a6b1e4061c02821336d4e0dd8361c6901
Shariful Haque <sharif.shuvo44200@gmail.com>
2023-09-25 12:10:36 +0600

    first commit

---
+++ app/Views/includes/footer.php
@@ -0,0 +1,14 @@
+<link href="<?php echo base_url();?>public/assets/css/bootsrap.min.css" rel="stylesheet" >
+<div class="card text-center">
+  <div class="card-header">
+    Featured
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  <div class="card-footer text-body-secondary">
+    2 days ago
+  </div>
+</div>
---
+++ app/Views/includes/header.php
@@ -0,0 +1,16 @@
+
+<link rel="stylesheet" href="<?php echo base_url('public/assets/css/bootstrap.css')?>" type="text/css">
+<ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Disabled</a>
+  </li>
+</ul>
